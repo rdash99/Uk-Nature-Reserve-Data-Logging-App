bbe6bd159f7ac02e09f5145bd75e2a3e1de8aa10
rdash99 <pokemonmaster9967@gmail.com>
2020-11-12 22:30:06 +0000

    Create login page frame and routes

---
+++ app/lib/home_route.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class HomeRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final menus = Column(
+      children: <Widget>[
+        ListTile(
+          title: Text("Logout"),
+          onTap: () {
+            Navigator.popUntil(context, ModalRoute.withName("/"));
+          },
+        ),
+      ],
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Welcome"),
+      ),
+      drawer: Drawer(
+        child: Column(
+          children: [
+            UserAccountsDrawerHeader(
+              accountName: Text("John Doe"),
+              accountEmail: Text("johndoe@testfairy.com"),
+            ),
+            menus
+          ],
+        ),
+      ),
+      body: Center(
+        child: Text("Hello"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: new Icon(Icons.ac_unit),
+        onPressed: () {},
+      ),
+    );
+  }
+}
---
+++ app/lib/login_route.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'home_route.dart';
+
+class LoginRoute extends StatefulWidget {
+  @override
+  State createState() => _LoginRouteState();
+}
+
+final icon = Image.asset(
+  'Images/lock.jpg',
+);
+
+class _LoginRouteState extends State<LoginRoute> {
+  @override
+  Widget build(BuildContext context) {
+    final username = Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: TextField(
+        decoration: InputDecoration(
+          labelText: "Username",
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(4.0),
+          ),
+        ),
+      ),
+    );
+    final password = Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: TextField(
+        obscureText: true,
+        decoration: InputDecoration(
+          labelText: "Password",
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(4.0),
+          ),
+        ),
+      ),
+    );
+    final submitButton = Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Material(
+        elevation: 5.0,
+        shadowColor: Colors.lime.shade100,
+        child: MaterialButton(
+          minWidth: 200.0,
+          height: 48.0,
+          child: Text(
+            "LOG IN",
+            style: TextStyle(color: Colors.white, fontSize: 16.0),
+          ),
+          color: Colors.lime,
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => HomeRoute()));
+          },
+        ),
+      ),
+    );
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: ListView(
+          children: [icon, username, password, submitButton],
+        ),
+      ),
+    );
+  }
+}
