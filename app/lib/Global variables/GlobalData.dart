5fabb16478a049cfebe5136d099ac8573058f550
rdash99 <pokemonmaster9967@gmail.com>
2020-12-03 19:07:32 +0000

    Add global variables + sign up functionality

---
+++ app/lib/Global variables/GlobalData.dart
@@ -0,0 +1,7 @@
+class GlobalData {
+  static var email = '';
+  static var password_1 = '';
+  static var password_2 = '';
+  static var password = '';
+  static var userID = '';
+}
--- app/lib/ui/sign_up_route.dart
+++ app/lib/ui/sign_up_route.dart
@@ -4,32 +4,46 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'login_route.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:app/Global variables/GlobalData.dart' as Globals;
+import 'home_route.dart';
 
 FirebaseAuth auth = FirebaseAuth.instance;
 
 class SignUpRoute extends StatefulWidget {
-  var password_1 = '';
   @override
   State createState() => _SignUpRouteState();
 }
 
 class _SignUpRouteState extends State<SignUpRoute> {
+  final _controller = TextEditingController();
+  bool _validate = false;
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     //create email input
     final inputEmail = Padding(
       padding: const EdgeInsets.all(16.0),
-      child: TextField(
+      child: TextFormField(
+        controller: _controller,
         decoration: InputDecoration(
           labelText: "Username",
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(4.0),
           ),
         ),
+        onChanged: (text) {
+          Globals.GlobalData.email = text;
+        },
       ),
     );
-    //create password input
-    final password = Padding(
+    //create first password input
+    final passwordInput1 = Padding(
       padding: const EdgeInsets.all(16.0),
       child: TextFormField(
         obscureText: true,
@@ -39,10 +53,33 @@ class _SignUpRouteState extends State<SignUpRoute> {
             borderRadius: BorderRadius.circular(4.0),
           ),
         ),
-        onChanged: (text) {},
+        onChanged: (text) {
+          Globals.GlobalData.password_1 = text;
+        },
       ),
     );
-    //create button
+    //create second password input
+    final passwordInput2 = Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: TextFormField(
+        obscureText: true,
+        decoration: InputDecoration(
+          labelText: "Password",
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(4.0),
+          ),
+        ),
+        onChanged: (text) {
+          Globals.GlobalData.password_2 = text;
+          if (Globals.GlobalData.password_1 == Globals.GlobalData.password_2) {
+            Globals.GlobalData.password = Globals.GlobalData.password_2;
+          } else {
+            return Text('Passwords do not match!');
+          }
+        },
+      ),
+    );
+    //create submission button
     final signUpButton = Padding(
       padding: const EdgeInsets.all(16.0),
       child: MaterialButton(
@@ -58,7 +95,13 @@ class _SignUpRouteState extends State<SignUpRoute> {
           try {
             UserCredential userCredential = await FirebaseAuth.instance
                 .createUserWithEmailAndPassword(
-                    email: "test@gmail.com", password: "SuperSecretPassword!");
+                    email: Globals.GlobalData.email,
+                    password: Globals.GlobalData.password);
+            //clear stored data on successful submission
+            Globals.GlobalData.email = '';
+            Globals.GlobalData.password_1 = '';
+            Globals.GlobalData.password_2 = '';
+            Globals.GlobalData.password = '';
           } on FirebaseAuthException catch (e) {
             if (e.code == 'weak-password') {
               print('The password provided is too weak.');
@@ -71,6 +114,26 @@ class _SignUpRouteState extends State<SignUpRoute> {
         },
       ),
     );
+    final homeButton = Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Material(
+        elevation: 5.0,
+        shadowColor: Colors.blue.shade100,
+        child: MaterialButton(
+          minWidth: 200.0,
+          height: 48.0,
+          child: Text(
+            "Go Back",
+            style: TextStyle(color: Colors.white, fontSize: 16.0),
+          ),
+          color: Colors.blue,
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => HomeRoute()));
+          },
+        ),
+      ),
+    );
     //display page
     return Scaffold(
       backgroundColor: Colors.white,
@@ -78,8 +141,10 @@ class _SignUpRouteState extends State<SignUpRoute> {
         child: ListView(
           children: [
             inputEmail,
-            password,
+            passwordInput1,
+            passwordInput2,
             signUpButton,
+            homeButton,
           ],
         ),
       ),
