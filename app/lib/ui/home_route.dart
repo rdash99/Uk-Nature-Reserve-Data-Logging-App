c1dccf8fd1a39ded793a16de78393c28c6779d2e
rdash99 <pokemonmaster9967@gmail.com>
2020-12-08 15:40:39 +0000

    Update home_route.dart

--- app/lib/ui/home_route.dart
+++ app/lib/ui/home_route.dart
@@ -1,9 +1,21 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'dart:html';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/src/widgets/framework.dart';
 
-class HomeRoute extends StatelessWidget {
+class HomeRoute extends StatefulWidget {
+  @override
+  State createState() => _HomeRouteState();
+}
+
+class _HomeRouteState extends State<HomeRoute> {
+  bool _isVisible1 = false;
+  bool _isVisible2 = true;
+
   @override
   Widget build(BuildContext context) {
-    final menus = Column(
+    final menu = Column(
       children: <Widget>[
         ListTile(
           title: Text("Identify"),
@@ -11,14 +23,41 @@ class HomeRoute extends StatelessWidget {
             Navigator.popUntil(context, ModalRoute.withName("/identify"));
           },
         ),
-        ListTile(
-          title: Text("Logout"),
-          onTap: () {
-            Navigator.popUntil(context, ModalRoute.withName("/"));
-          },
+        //log out button
+        Visibility(
+          visible: _isVisible1,
+          child: ListTile(
+            title: Text("Logout"),
+            onTap: () {
+              Navigator.popUntil(context, ModalRoute.withName("/"));
+            },
+          ),
+        ),
+        //login button
+        Visibility(
+          visible: _isVisible2,
+          child: ListTile(
+            title: Text("Login"),
+            onTap: () {
+              Navigator.popUntil(context, ModalRoute.withName("/"));
+            },
+          ),
         ),
       ],
     );
+    FirebaseAuth.instance.authStateChanges().listen((User user) {
+      if (user == null) {
+        setState(() {
+          _isVisible2 = true;
+          _isVisible1 = false;
+        });
+      } else {
+        setState(() {
+          _isVisible2 = false;
+          _isVisible1 = true;
+        });
+      }
+    });
 
     return Scaffold(
       appBar: AppBar(
@@ -31,7 +70,7 @@ class HomeRoute extends StatelessWidget {
               accountName: Text("John Doe"),
               accountEmail: Text("johndoe@testfairy.com"),
             ),
-            menus
+            menu,
           ],
         ),
       ),
