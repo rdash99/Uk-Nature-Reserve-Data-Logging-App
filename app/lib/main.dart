ebad36c06460eaebe57c2514ea355c00151f5561
rdash99 <pokemonmaster9967@gmail.com>
2021-02-09 12:58:11 +0000

    Removed import statements from main.dart

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -8,8 +8,6 @@ import 'ui/add_sightings_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:hive/hive.dart';
-import 'package:path_provider/path_provider.dart' as path_provider;
 
 FirebaseAnalytics analytics;
 Future<void> main() async {
