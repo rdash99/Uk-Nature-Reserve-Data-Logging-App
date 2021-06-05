9022bdf801a1737b1e42285d190d2dc633dd67e6
rdash99 <pokemonmaster9967@gmail.com>
2021-06-05 19:58:47 +0100

    Update main.dart

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:app/ui/Sightings/edit_sightings_page.dart';
 import 'package:app/ui/Sightings/EditPages/edit_sightings_page_butterflies.dart';
 import 'package:app/ui/Sightings/EditPages/edit_sightings_page_birds.dart';
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 import 'ui/login_route.dart';
 import 'ui/home_route.dart';
 import 'ui/sign_up_route.dart';
@@ -12,10 +13,14 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'Page_navigation/tabs_page.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
 
 FirebaseAnalytics analytics;
 Future<void> main() async {
   await WidgetsFlutterBinding.ensureInitialized();
+  final String DocsPath =
+      await path_provider.getApplicationDocumentsDirectory().toString();
+  await Hive.init(DocsPath);
   analytics = FirebaseAnalytics();
   await Firebase.initializeApp();
   runApp(App());
