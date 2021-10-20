36359f5c291026c7a7cbff682258b798898f8fd3
Rowan Dash <pokemonmaster9967@gmail.com>
2021-10-20 20:00:15 +0100

    Move stuff for heroku build

--- app/Data sets and lists/Python scripts/csv scripts/Bird data parse bou A-C.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-f=pd.read_csv("app/Data sets and lists/Python scripts/csv scripts/bou.csv")
-keep_col = ['British name', 'Scientific name', 'Thumbnail', 'Images', 'Description']
-new_f = f[keep_col]
-new_f.to_csv('app/Data sets and lists/Python scripts/csv scripts/Birds A-C Parsed.csv', index=False)
