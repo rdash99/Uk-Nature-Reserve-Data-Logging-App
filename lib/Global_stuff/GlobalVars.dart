2258fef533420b4686f901834aadf8cafa5ca15f
Rowan Dash <pokemonmaster9967@gmail.com>
2021-10-20 20:05:55 +0100

    Move stuff

---
+++ lib/Global_stuff/GlobalVars.dart
@@ -0,0 +1,13 @@
+class GlobalData {
+  static var email = '';
+  static var password_1 = '';
+  static var password_2 = '';
+  static var password = '';
+  static var userID = '';
+  static var Username = '';
+  static var firstName = '';
+  static var surname = '';
+  static var butterBird = '';
+  static int butterflyNum = 0;
+  static var birdNum = '';
+}
