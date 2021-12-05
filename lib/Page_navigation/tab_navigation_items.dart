7f114bf77e426e2181aa2c5ba59d551820538a8d
Rowan Dash <pokemonmaster9967@gmail.com>
2021-12-05 14:52:06 +0000

    Update tab_navigation_items.dart
    Visibility testing

--- lib/Page_navigation/tab_navigation_items.dart
+++ lib/Page_navigation/tab_navigation_items.dart
@@ -8,38 +8,47 @@ import 'package:app/ui/login_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:app/ui/test_page.dart';
+import 'package:flutter_settings_screens/flutter_settings_screens.dart';
 
 class TabNavigationItem {
   final Widget page;
   final String title;
   final Icon icon;
+  final bool visible;
 
   TabNavigationItem({
     required this.page,
     required this.title,
     required this.icon,
+    required this.visible,
   });
 
   static List<TabNavigationItem> get items => [
         TabNavigationItem(
             page: HomeRoute(),
             title: "Home",
-            icon: Icon(Icons.home, color: Colors.black)),
+            icon: Icon(Icons.home, color: Colors.black),
+            visible: true),
         TabNavigationItem(
             page: AddSightingsRoute(),
             title: "Add a sighting",
-            icon: Icon(Icons.add_circle, color: Colors.black)),
+            icon: Icon(Icons.add_circle, color: Colors.black),
+            visible: true),
         TabNavigationItem(
             page: TestRoute(),
             title: "Test",
-            icon: Icon(Icons.edit, color: Colors.black)),
+            icon: Icon(Icons.edit, color: Colors.black),
+            visible: Settings.getValue<bool>(
+                'key-switch-experimental-features', false)),
         TabNavigationItem(
             page: IdentificationRoute(),
             title: "Identification",
-            icon: Icon(Icons.search, color: Colors.black)),
+            icon: Icon(Icons.search, color: Colors.black),
+            visible: true),
         TabNavigationItem(
             page: SettingsRoute2(),
             title: "Settings",
-            icon: Icon(Icons.settings, color: Colors.black)),
+            icon: Icon(Icons.settings, color: Colors.black),
+            visible: true),
       ];
 }
