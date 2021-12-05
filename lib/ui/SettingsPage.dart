fbdfca339995ebed79a01b484a28ee65130d1ecc
Rowan Dash <pokemonmaster9967@gmail.com>
2021-12-05 15:50:51 +0000

    Update SettingsPage.dart

--- lib/ui/SettingsPage.dart
+++ lib/ui/SettingsPage.dart
@@ -38,7 +38,7 @@ class _SettingsRouteState extends State<SettingsRoute> {
             leading: Icon(MdiIcons.testTube),
             settingKey: 'key-test-page',
             title:
-                'Test page, things in development use at your own risk - Warning causes uncontrollable flickering when enabled for some reason, I do not know why',
+                'Test page, things in development use at your own risk - Warning causes uncontrollable flickering when enabled for some reason',
             onChange: (value) {
               debugPrint('key-test-page: $value');
             },
