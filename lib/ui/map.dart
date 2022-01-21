5976dc54148aab173efa22a21c4a68cb5a14c9f3
Rowan Dash <pokemonmaster9967@gmail.com>
2022-01-21 22:00:25 +0000

    Update map.dart

--- lib/ui/map.dart
+++ lib/ui/map.dart
@@ -1,3 +1,4 @@
+import 'package:geoflutterfire/geoflutterfire.dart';
 import 'package:syncfusion_flutter_maps/maps.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_settings_screens/flutter_settings_screens.dart';
@@ -10,14 +11,17 @@ class MapRoute extends StatefulWidget {
 }
 
 class _MapRouteState extends State<MapRoute> {
+  final geo = Geoflutterfire();
   var data = MapData().data;
   late MapShapeSource _mapSource;
   late MapZoomPanBehavior _zoomPanBehavior;
   int selectedIndex = -1;
+  late MapShapeLayerController _controller;
 
   @override
   void initState() {
     _zoomPanBehavior = MapZoomPanBehavior();
+    _controller = MapShapeLayerController();
     _mapSource = MapShapeSource.asset(
       'assets/uk3.json',
       shapeDataField: "LAD21NM",
@@ -47,9 +51,18 @@ class _MapRouteState extends State<MapRoute> {
                     } else {
                       selectedIndex = index;
                     }
-                    print(selectedIndex);
+                    //print(selectedIndex);
                   });
                 },
+                initialMarkersCount: 0,
+                markerBuilder: (BuildContext context, int index) {
+                  return MapMarker(
+                    latitude: _data[index].latitude,
+                    longitude: _data[index].longitude,
+                    child: Icon(Icons.add_location),
+                  );
+                },
+                controller: _controller,
                 loadingBuilder: (BuildContext context) {
                   return Container(
                     height: 25,
